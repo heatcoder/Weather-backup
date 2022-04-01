4ed950d0f1737af284af1fb50d6c675a5fd1c91a
heatcoder <100113591+heatcoder@users.noreply.github.com>
2022-04-01 14:17:17 +0800

    Final

--- website/app.js
+++ website/app.js
@@ -3,6 +3,7 @@ const content = document.getElementById('content')
 const feelingsField = document.getElementById('feelings')
 const dateField = document.getElementById('date')
 const temperatureField = document.getElementById('temp')
+
 // Dynamically create a new JS date instance
 let d = new Date();
 let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
@@ -13,28 +14,28 @@ const baseLink = 'http://api.openweathermap.org/data/2.5/weather?'
 
 //Zip number constant
 const zipNumber = document.getElementById('zip'); 
-
-const openWeather = async function (baseLink, zip = '', api) {
+console.log(zipNumber)
+const openWeather = async function (baseLink, zipNumber = '', key) {
+  
   //defining url
-  const url = `${baseLink}zip=${zip}${api}` 
+  const url = `${baseLink}zip=${zipNumber}${key}` 
+
   let title=document.querySelector('#title1');
-  return fetch(url)
-  .then(response => {
+  return fetch(url).then(response => {
     if (response.status == 200) {
-      return response.json();
+      return response.json(); 
     } else {
-      setTimeout(warn, 10)
+      setTimeout(warn, 10);
       function warn () {
-        title.innerHTML="Please enter all fields"
-        setTimeout(warnWillDisapear, 3000)
+        title.innerHTML="Please enter valid input"
+        setTimeout(warnWillDisapear, 3000);
           function warnWillDisapear () {
           title.innerHTML="Most Recent Entry ";
         }}
       throw new Error(response.status);
+   
     }
   });
-
- 
 }
 
 // custom data by user
@@ -53,21 +54,22 @@ const postServer = async function (path, data = {}) {
 }
 
 
-//button  async function, await for waiting data from server
+//button  asynchronus function, sending and receiving data from server
 const button = async function () { 
   const newData = await openWeather(baseLink, zipNumber.value, key)
   const data = { date: newDate, temperature: newData.main.temp, feelingsData: feelingsField.value}
   await postServer(('http://localhost:3000'), data) 
+
   .then(async function () {
     const response = await fetch(('http://localhost:3000/all'));
     const jsonText = await response.json()
-    
     dateField.innerHTML = jsonText.date;
     content.innerHTML = jsonText.feelingsPost;
   
     // changed temparature to degree celcius
     temperatureField.innerHTML = Math.floor((jsonText.temperature)-273.15);     
   });
+  
 } 
 
 
